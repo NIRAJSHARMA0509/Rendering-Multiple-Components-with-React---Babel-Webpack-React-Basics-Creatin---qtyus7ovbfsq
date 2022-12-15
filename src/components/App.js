e1b9122f1229e95cb40cb9a7721fd71822ea13a1
NIRAJSHARMA0509 <120631030+NIRAJSHARMA0509@users.noreply.github.com>
2022-12-15 14:08:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <React.Fragment>
-    <h1 data-ns-test = "project-name">Expense Calculator</h1>
-    <p data-ns-test = "project-description">Built using ReactJS</p>
-    </React.Fragment>
+    <div id="main">
+    <h1 data-ns-test="project-name">my.newtonschool.co</h1>
+    <p data-ns-test="project-description">Basically a website which could be used to practice for CS PROGAM </p>
+    </div>
   )
 }
+
+
+export default App;
